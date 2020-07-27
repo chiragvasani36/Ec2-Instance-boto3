605c50eb9e0731a0cb3e6cd486bdb75f6e9f848b
Chirag Vasani <55270861+chiragvasani36@users.noreply.github.com>
2020-07-27 17:57:07 +0530

    Update ec2_resource.py

--- ec2_resource.py
+++ ec2_resource.py
@@ -1,3 +1,6 @@
+import os
+print (os.environ.get('Password'))
+"""
 import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.')
@@ -7,7 +10,7 @@ parser.add_argument('--pass1',  help='value of pass')
 args = parser.parse_args()
 print(args.pass1)
 
-"""import boto3
+import boto3
 ec2_resource=boto3.resource('ec2')
 response=ec2_resource.create_instances(
     ImageId='ami-08f3d892de259504d',

20db03704e16fc5d3dd1cf751d430f501307d2e1
Chirag Vasani <55270861+chiragvasani36@users.noreply.github.com>
2020-07-27 14:39:49 +0530

    Create ec2_resource.py

---
+++ ec2_resource.py
@@ -0,0 +1,9 @@
+import boto3
+ec2_resource=boto3.resource('ec2')
+response=ec2_resource.create_instances(
+    ImageId='ami-08f3d892de259504d',
+    InstanceType='t2.micro',
+    MaxCount=1,
+    MinCount=1
+)
+print(response)

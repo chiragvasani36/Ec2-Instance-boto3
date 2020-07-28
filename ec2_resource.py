854442994f486208ef8958892acc76b955bbb4c0
Chirag Vasani <55270861+chiragvasani36@users.noreply.github.com>
2020-07-28 10:02:48 +0530

    Update ec2_resource.py

--- ec2_resource.py
+++ ec2_resource.py
@@ -1,7 +1,9 @@
 import os
 import boto3
-ec2_session =boto3.session.Session(aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),region_name='us-east-1')
-ec2_resource=ec2_session.resource('ec2')
+print(os.environ.get('SECRET_PASS'))
+print(os.environ.get('NOT_SEC'))
+
+ec2_resource=boto3.resource('ec2',aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),region_name='us-east-1')
 response=ec2_resource.create_instances(
     ImageId='ami-08f3d892de259504d',
     InstanceType='t2.micro',
